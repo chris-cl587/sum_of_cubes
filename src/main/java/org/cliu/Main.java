9f4bc489ebb0de35f225da236bb49bf3c8c51d39
chris <chris.cl587@gmail.com>
2020-09-20 15:51:11 -0700

    Small improvements

--- src/main/java/org/cliu/Main.java
+++ src/main/java/org/cliu/Main.java
@@ -1,8 +1,12 @@
 package org.cliu;
 
 
+import jvm_alloc_rate_meter.MeterThread;
+
 class Main {
     public static void main(String[] args) {
+        MeterThread t = new MeterThread((r) -> System.out.println("Rate is: " + (r / 1e6) + " MB/sec"));
+        t.start();
         Runner.run();
     }
 }
